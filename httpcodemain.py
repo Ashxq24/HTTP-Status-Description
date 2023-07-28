97021b56fa36eb3878095a6f3419543e005d8a16
Ashxq24 <139358699+Ashxq24@users.noreply.github.com>
2023-07-28 09:57:39 -0400

    Add files via upload

---
+++ httpcodemain.py
@@ -0,0 +1,6 @@
+import HTTPStatusCodes
+response_codes=input("enter the status code of the HTTP server : ")
+if response_codes in HTTPStatusCodes.codes:
+   print ("code description: ", HTTPStatusCodes.codes[response_codes])
+else:
+    print("Sorry , check if the entered status code of the HTTP server is genuine")
